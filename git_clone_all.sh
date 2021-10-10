bb3d9b3f5d49d5595fe00afc36baeb3eaf38c36d
tonyonifo <62163868+tonyonifo@users.noreply.github.com>
2021-10-10 12:04:53 +0200

    Create git_clone_all.sh
    Task 00

---
+++ git_clone_all.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/scopeInfinity/Video2Description.git \
+& git clone https://github.com/C-Anirudh/Drona \
+& git clone https://github.com/sveneschlbeck/A2T \
+& git clone https://github.com/rn-snehapriya/Automatic-Note-Taking-From-Video-Using-Tesseract-OCR \
+& git clone https://github.com/imanom/On-Premise-Speech-to-Text \
